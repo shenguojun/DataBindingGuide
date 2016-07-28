4a623e8cec2e7d60a79a31128bdb364d33d25655
Guojun Shen <shengj@rd.netease.com>
2016-07-28 17:59:47 +0800

    mod: 添加BindingMethods用法

--- app/src/main/java/com/xshen/databindingguide/ui/CustomTextView.java
+++ app/src/main/java/com/xshen/databindingguide/ui/CustomTextView.java
@@ -1,6 +1,8 @@
 package com.xshen.databindingguide.ui;
 
 import android.content.Context;
+import android.databinding.BindingMethod;
+import android.databinding.BindingMethods;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -13,6 +15,10 @@ import com.xshen.databindingguide.util.FontCache;
  * @version 创建时间：2016-07-21 22:53
  */
 
+@BindingMethods({
+        @BindingMethod(type = CustomTextView.class,
+                attribute = "app:customText",
+                method = "setMyCustomText")})
 public class CustomTextView extends TextView {
     public CustomTextView(Context context) {
         super(context);
@@ -31,7 +37,7 @@ public class CustomTextView extends TextView {
     }
 
 
-    public void setCustomText(CharSequence text) {
+    public void setMyCustomText(CharSequence text) {
         setText("Custom " + text);
     }
 }
